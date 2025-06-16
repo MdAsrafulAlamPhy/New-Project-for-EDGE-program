ca6ea5c0db74cb202f26c371994280e919f60392
MdAsrafulAlamPhy <asrafulalam11996@gmail.com>
2025-06-16 18:29:49 +0600

    Plotting graph on different categories

---
+++ University ranking.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('C:\python\cwurData.csv')
+
+universities_by_country = df.groupby('country')['institution'].nunique().reset_index()
+universities_by_country = universities_by_country.rename(columns={'institution': 'total_universities'})
+
+# Sort by number of universities (optional)
+universities_by_country = universities_by_country.sort_values(by='total_universities', ascending=False)
+
+# Plot
+plt.figure(figsize=(12, 8))
+sns.barplot(x='country', y='total_universities', data=universities_by_country, palette='viridis')
+
+plt.title('Total Number of Universities by Country')
+plt.xlabel('Country')
+plt.ylabel('Total Universities')
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.show()
+
+# Group by country and sum patents
+patents_by_country = df.groupby('country')['patents'].sum().reset_index()
+
+# Sort values for better visualization (optional)
+patents_by_country = patents_by_country.sort_values(by='patents', ascending=False)
+plt.figure(figsize=(12, 8))
+sns.barplot(x='country', y='patents', data=patents_by_country, palette='viridis')
+
+plt.title('Total Number of Patents by Country')
+plt.xlabel('Country')
+plt.ylabel('Total Patents')
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.show()
